41ee9d537b9b4ed95ecfe4706d31802912ad3ce6
Desenvolvimento15 <96912707+Desenvolvimento15@users.noreply.github.com>
2024-02-15 10:33:12 -0300

    vs01

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
-$('.galeria').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  arrows: false,
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear',
-  lazyLoad: 'ondemand', controls: false,
-   
+$(document).ready(function() {
+  const $slides = $('#slides');
+  const $dotsContainer = $('#dots-container');
+  const $textItem = $('#text-item');
+
+  const slideTexts = ["Roxo Claro", "Azul", "Verde"];
+
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    $slides.css('transform', `translateX(${-index * 100}%)`);
+    $textItem.text(slideTexts[index]);
+
+    $('.dot').each(function(i, dot) {
+      $(dot).toggleClass('active', i === index);
+    });
+  }
+
+  function createDots() {
+    for (let i = 0; i < slideTexts.length; i++) {
+      const $dot = $('<span>').addClass('dot').on('click', () => showSlide(i));
+      $dotsContainer.append($dot);
+    }
+  }
+
+  function autoPlay() {
+    setInterval(() => {
+      currentSlide = (currentSlide + 1) % slideTexts.length;
+      showSlide(currentSlide);
+    }, 3000); // Altera o slide a cada 3 segundos
+  }
+
+  createDots();
+  showSlide(currentSlide);
+  autoPlay();
 });
