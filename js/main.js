b2d39f8dbc4da9cd9c332d2b693ba07bf20a9795
h3110 <desenvolvimento15@dl.com.br>
2024-03-07 17:02:54 -0300

    vs01

--- js/main.js
+++ js/main.js
@@ -34,3 +34,40 @@ $(document).ready(function() {
   showSlide(currentSlide);
   autoPlay();
 });
+
+$(document).ready(function() {
+  const $slides = $('#slides');
+  const $dotsContainer = $('#dots-container-desk');
+  const $textItem = $('#text-item-desk');
+
+  const slideTexts = ["Roxo Claro", "Azul", "Verde"];
+
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    $slides.css('transform', `translateX(${-index * 100}%)`);
+    $textItem.text(slideTexts[index]);
+
+    $('.dot').each(function(i, dot) {
+      $(dot).toggleClass('active', i === index);
+    });
+  }
+
+  function createDots() {
+    for (let i = 0; i < slideTexts.length; i++) {
+      const $dot = $('<span>').addClass('dot').on('click', () => showSlide(i));
+      $dotsContainer.append($dot);
+    }
+  }
+
+  function autoPlay() {
+    setInterval(() => {
+      currentSlide = (currentSlide + 1) % slideTexts.length;
+      showSlide(currentSlide);
+    }, 3000); // Altera o slide a cada 3 segundos
+  }
+
+  createDots();
+  showSlide(currentSlide);
+  autoPlay();
+});
